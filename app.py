bfa2a1f8bf1fe1b1d05c417899055130563bb0cd
TrustiestCoder-0 <poulsenmatheus@gmail.com>
2026-05-10 13:17:18 +0900

    adicionado nome e idade

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Olá mundo")
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+with open("dados.txt", "w") as arquivo:
+    arquivo.write(f"Nome: {nome}\n")
+    arquivo.write(f"Idade: {idade}")
+
+print("Dados salvos com sucesso!")
