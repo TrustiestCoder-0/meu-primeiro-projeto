d8492934dc799101446c6a5c2de012aed6212325
TrustiestCoder-0 <poulsenmatheus@gmail.com>
2026-05-10 13:50:07 +0900

    Foi colocado nova maneira de salvar o nome e idade do usuario

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-print("Oi, tudo bem?")
-RESPOSTA = input()
-print("Bem vindo ao mundo da programação")                               
-NOME = input("QUAL O SEU NOME PROGRAMADOR?")
+print('Oi, tudo bem?')
 
-IDADE = input("QUAL A SUA IDADE PROGRAMADOR?") 
+resposta = input()
 
-print('Uau', NOME + '!', 'Você é muito novo para um programador tão bom!')
+print('Que legal!')
+
+nome = input('Qual é o seu nome, programador? ')
+
+idade = input('Qual é a sua idade? ')
+
+with open('dados.txt', 'w') as arquivo:
+    arquivo.write(f'Nome: {nome}\n')
+    arquivo.write(f'Idade: {idade}')
+
+print('Dados salvos com sucesso!')
+
+print('Uau', nome + '!', 'Você é muito novo para um programador tão bom!')
